3f6bc21c59f7c1bf0c8bc07e49dc2b4fcfc948ef
bdushime <bdushime47@gmail.com>
2025-10-01 22:49:22 +0200

    style: update menu bar styling

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,9 +1,11 @@
 function MenuBar() {
   return (
     <div className="flex h-16 w-full flex-row bg-black gap-8">
-      <div className="h-16 w-4 flex-1 bg-yellow-500"></div>
-      <div className="h-16 w-4 flex-1 bg-red-950"></div>
-      <div className="h-16 w-4 flex-1 bg-green-800"></div>
+      <div className="h-16 w-4 flex flex-1 items-center justify-center text-xl text-white">Menu</div>
+      <div className="absolute left-1/2 -bottom-4 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full   bg-black text-white">
+  Car
+</div>
+      <div className="h-16 w-4 flex  flex-1 items-center justify-center text-xl text-white">Profile</div>
     </div>
   )
 }
