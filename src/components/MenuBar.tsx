2e23548856365717b669e38650a804cb40e0a4c3
bdushime <bdushime47@gmail.com>
2025-10-02 11:19:01 +0200

    style: update menu bar styling

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,17 +1,19 @@
-import CarIcon from "../assets/CarIcon"
-import ProfileIcon from "../assets/ProfileIcon"
+import CarIcon from '../assets/CarIcon'
+import ProfileIcon from '../assets/ProfileIcon'
 
 function MenuBar() {
   return (
-    <div className="flex h-16 w-full flex-row gap-8 bg-[#111827] rounded-b-lg">
-      <div className="flex h-16 w-4 flex-1 items-center justify-start ml-6 text-xl text-white">
+    <div className="flex h-16 w-full flex-row gap-8 rounded-b-lg bg-[#111827]">
+      <div className="ml-6 flex h-16 w-4 flex-1 items-center justify-start text-xl text-white">
         Menu
       </div>
-      <div className="absolute -bottom-4 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full   bg-[#111827] text-white">
-        <CarIcon className="h-8 w-8"/>
+
+      <div className="absolute -bottom-4 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full bg-[#111827] text-white">
+        <CarIcon className="h-8 w-8" />
       </div>
-      <div className="flex h-16 w-4  flex-1 items-center justify-end mr-6 text-xl text-white">
-        <ProfileIcon className="h-8 w-8"/>
+
+      <div className="mr-6 flex h-16 w-4 flex-1 items-center justify-end text-xl text-white">
+        <ProfileIcon className="h-8 w-8" />
       </div>
     </div>
   )
