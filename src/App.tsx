327a94c41b54092f8012d5259da962c1770259e3
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-10-02 10:23:28 +0200

    feat: implementing routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 // Configure axios hooks
 // Do not delete this if you want to use the provided API hooks in `src/hooks`
@@ -11,11 +13,11 @@ configure({
 
 function App(): ReactElement {
   return (
-    <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
-      <h1 className="text-4xl font-bold">Hello to CarSharing</h1>
-      <p>If you can read this, you have successfully started the base frontend repository!</p>
-      <p>Happy coding!</p>
-    </main>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
-const HomePage = () => <div>HomePage</div>
+const HomePage = () => (
+  <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
+    <h1 className="text-4xl font-bold">Hello to CarSharing</h1>
+    <p>If you can read this, you have successfully started the base frontend repository!</p>
+    <p>Happy coding!</p>
+  </main>
+)
 
 export default HomePage
