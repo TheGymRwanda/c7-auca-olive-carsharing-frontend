f0c70b37f891d4882fc9b55a6196676cab66bc16
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-10-02 19:35:02 +0200

    fix: folder structure

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,3 @@
-const HomePage = () => (
-  <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
-    <h1 className="text-4xl font-bold">Hello to CarSharing</h1>
-    <p>If you can read this, you have successfully started the base frontend repository!</p>
-    <p>Happy coding!</p>
-  </main>
-)
+const HomePage = () => <></>
 
 export default HomePage
