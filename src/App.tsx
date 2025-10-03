532160460c72f7544387ed2eda91971b437ee67e
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-03 12:02:03 +0200

    feat: added icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
+import CarDetails from './components/CarDetails'
 
-// Configure axios hooks
-// Do not delete this if you want to use the provided API hooks in `src/hooks`
 configure({
   defaultOptions: {
     autoCancel: false,
@@ -11,11 +10,9 @@ configure({
 
 function App(): ReactElement {
   return (
-    <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
-      <h1 className="text-4xl font-bold">Hello to CarSharing</h1>
-      <p>If you can read this, you have successfully started the base frontend repository!</p>
-      <p>Happy coding!</p>
-    </main>
+    <section className="mx-auto flex min-h-screen justify-center items-center flex-col gap-8 py-10 bg-[#265e78] text-white">
+      <CarDetails />
+    </section>
   )
 }
 
---
+++ src/components/CarDetails.tsx
@@ -0,0 +1,43 @@
+import CarIcon from '../assets/CarIcon'
+import FuelIcon from '../assets/FuelIcon'
+import HorseIcon from '../assets/HorseIcon'
+import ProfileIcon from '../assets/ProfileIcon'
+import danger from '../assets/dangerIcon.png'
+import plateNumber from '../assets/plateNumberIcon.png'
+import yellowCar from '../assets/yellowCar.png'
+
+const CarDetails = () => {
+  return (
+    <div className="p-10">
+      <h1>DETAILS</h1>
+      <img src={yellowCar} className="w-[356] and h-[198]" alt="" />
+      <h3>Tini Titan</h3>
+      <div className="flex gap-2">
+        <ProfileIcon />
+        <span>Anna</span>
+      </div>
+      <div className="flex gap-2">
+        <CarIcon />
+        <span>Countryman</span>
+      </div>
+      <div className="flex gap-2">
+        <img src={plateNumber} alt="Plate number icon" />
+        <span>M-LK-3456</span>
+      </div>
+      <div className="flex gap-2">
+        <HorseIcon />
+        <span>122hp</span>
+      </div>
+      <div className="flex gap-2">
+        <FuelIcon />
+        <span>Petrol</span>
+      </div>
+      <div className="flex gap-2">
+        <img src={danger} alt="No smoking icon" />
+        <span className="font-bold">No Smoking</span>
+      </div>
+    </div>
+  )
+}
+
+export default CarDetails
