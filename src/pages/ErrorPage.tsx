79c8ff6fffd7b4ef2258213b62ec13e852e867a6
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-03 13:39:04 +0200

    fix: solved the conflicts

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,10 @@
+import Error from '../components/Error'
+
 const ErrorPage = () => (
   <div>
     <section className="mx-auto flex min-h-screen flex-col items-center justify-between gap-8 bg-[#265e78] py-10">
-      <ErrorPage />
+      <Error />
     </section>
   </div>
 )
-
 export default ErrorPage
