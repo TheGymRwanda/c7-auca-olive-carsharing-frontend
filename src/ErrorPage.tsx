2150ee6581543ccb4c1d388c9d77db92b9cd00f7
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-03 09:50:53 +0200

    refactor: added an event listener on the back button

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,17 +1,27 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 import unionImage from './assets/Union.png'
 
-const ErrorPage: FC = () => (
-  <div className="p-8 text-center text-white" style={{ fontFamily: "'Playfair Display', serif" }}>
-    <h1 className="mb-8 text-5xl font-bold">OOOOOPS!</h1>
-    <img src={unionImage} alt="Smoking car image" className="mx-auto" />
-    <div className="p-4 text-center text-xl">
-      <p className="mt-4 font-normal">Something went wrong.</p>
-      <p className="font-normal">We will solve your issue soon.</p>
-    </div>
+const ErrorPage: FC = () => {
+  const navigate = useNavigate()
 
-    <button className="mt-8 w-full rounded-2xl bg-white px-4 py-2 text-[#265e78]">Go back</button>
-  </div>
-)
+  return (
+    <div className="p-8 text-center text-white" style={{ fontFamily: "'Playfair Display', serif" }}>
+      <h1 className="mb-8 text-5xl font-bold">OOOOOPS!</h1>
+      <img src={unionImage} alt="Smoking car image" className="mx-auto" />
+      <div className="p-4 text-center text-xl">
+        <p className="mt-4 font-normal">Something went wrong.</p>
+        <p className="font-normal">We will solve your issue soon.</p>
+      </div>
+
+      <button
+        onClick={() => navigate('/')}
+        className="mt-8 w-full rounded-2xl bg-white px-4 py-2 text-[#265e78]"
+      >
+        Go back
+      </button>
+    </div>
+  )
+}
 
 export default ErrorPage
