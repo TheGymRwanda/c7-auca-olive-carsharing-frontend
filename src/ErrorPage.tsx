2085f1c22c09727880cceb0d25083fb352d43ddb
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-03 09:48:10 +0200

    refactor: used import for the image

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
+import unionImage from './assets/Union.png'
 
 const ErrorPage: FC = () => (
   <div className="p-8 text-center text-white" style={{ fontFamily: "'Playfair Display', serif" }}>
     <h1 className="mb-8 text-5xl font-bold">OOOOOPS!</h1>
-    <img src="/src/assets/Union.png" alt="Smoking car image" className="mx-auto" />
+    <img src={unionImage} alt="Smoking car image" className="mx-auto" />
     <div className="p-4 text-center text-xl">
       <p className="mt-4 font-normal">Something went wrong.</p>
       <p className="font-normal">We will solve your issue soon.</p>
