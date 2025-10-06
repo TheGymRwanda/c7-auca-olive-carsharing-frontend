c6aab4854f0644e62e0a2e07dba71fb81b0eb76a
bdushime <bdushime47@gmail.com>
2025-10-06 10:30:26 +0200

    style: update drawe  styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,7 @@ configure({
 
 function App(): ReactElement {
   return (
-    // <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
-    //   <h1 className="text-4xl font-bold">Hello to CarSharing</h1>
-    //   <p>If you can read this, you have successfully started the base frontend repository!</p>
-    //   <p>Happy coding!</p>
-    // </main>
+   
     <MenuBar />
   )
 }
