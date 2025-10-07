45dda0a88186f64093fa830bea1a1f7d1c91ed29
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-10-07 10:52:18 +0200

    feat: homepage and title component

---
+++ src/components/MoniShareTitle.tsx
@@ -0,0 +1,8 @@
+const MoniShareTitle = () => (
+  <div className="text-white">
+    <h1>Moni</h1>
+    <h2>Share</h2>
+  </div>
+)
+
+export default MoniShareTitle
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,11 @@
-const HomePage = () => <></>
+import MoniShareTitle from '../components/MoniShareTitle'
+
+const HomePage = () => (
+  <>
+    <main className="bg-[#265e78]">
+      <MoniShareTitle />
+    </main>
+  </>
+)
 
 export default HomePage
