035f2667fdc8e6e8dc6fd49ada95b45364fbc625
kamufozi <kamufozi679@gmail.com>
2025-10-08 13:12:29 +0200

    refactor: removed an unnecesarry line

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
 import { AppRoutes } from './routes'
-import BackButton from './components/BackButton'
 
 configure({
   defaultOptions: {
