21c954d99257954c6c29e746d176983d60a8042e
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-09 20:41:14 +0200

    refactor: added the menu bar

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -19,10 +19,7 @@ const Error: FC = () => {
           <p className="font-normal">We will solve your issue soon.</p>
         </div>
       </div>
-      <Button
-        onClick={() => navigate('/')}
-        className="mt-6 w-[90%] max-w-sm rounded-2xl bg-white text-[#265e78] text-lg mb-8 "
-      >
+      <Button onClick={() => navigate('/')} className="mt-6 w-[90%] max-w-sm text-[#265e78] mb-20">
         Go back
       </Button>
     </>
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,13 +1,12 @@
-import Error from '../components/Error'
 import MenuBar from '../components/MenuBar'
-
+import Error from '../components/Error'
 const ErrorPage = () => (
-  <>
-    {/* <MenuBar /> */}
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#265e78]">
+  <div className="flex min-h-screen flex-col bg-[#265e78]">
+    <MenuBar />
+    <div className="flex flex-1 flex-col items-center justify-center">
       <Error />
     </div>
-  </>
+  </div>
 )
 
 export default ErrorPage
