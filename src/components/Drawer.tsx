8902a88a35eac67f9a9c3d47e1d8d05267a086eb
bdushime <bdushime47@gmail.com>
2025-10-09 21:13:43 +0200

    style: aligning items as the figma file and adding hover

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -6,21 +6,22 @@ import Stairs from '../assets/Stairs.svg'
 import List from '../assets/List.svg'
 
 const Drawer = () => (
-  <div className="ml-4 mt-6 flex h-80 w-52 flex-1 flex-col items-center justify-center rounded-lg bg-[#3e7591] ">
-    <div className="mt-4 flex h-full w-full flex-col space-y-3  rounded-t-lg bg-[#3e7591] pl-8 text-white">
+  <div className="ml-4 mt-6 flex h-72 w-52  flex-col items-center justify-center rounded-lg bg-[#3e7591] ">
+    <div className="mt-4 flex h-44 w-full flex-col space-y-3  rounded-t-lg bg-[#3e7591] pl-8 text-white">
       <div className="flex flex-row space-x-4 hover:bg-white hover:bg-opacity-20">
         <CarIcon />
         <p>Book A Car</p>
       </div>
-      <div className="flex flex-row space-x-4 hover:bg-white hover:bg-opacity-20">
+      <div className="flex flex-row space-x-4  hover:bg-white hover:bg-opacity-20">
         <img src={Stairs} />
         <p>My Bookings</p>
       </div>
     </div>
-    <div className="w-full bg-[#3e7591] px-4">
+    <div className="w-full bg-[#3e7591] px-4 mt-2">
       <div className="border-b border-gray-300"></div>
     </div>
-    <div className="mb-3 mt-4 flex h-full w-full flex-col space-y-3 bg-[#3e7591] pl-8 text-white ">
+    <div className="mb-3 mt-4 flex h-44 w-full flex-col space-y-3 bg-[#3e7591] pl-8 text-white ">
+      <div className="font-medium">My cars</div>
       <div className="flex flex-row space-x-4 hover:bg-white hover:bg-opacity-20">
         <img src={CarsM} />
         <p>See My Cars</p>
@@ -37,8 +38,8 @@ const Drawer = () => (
     <div className="w-full bg-[#3e7591]  px-4">
       <div className="border-b border-gray-300"></div>
     </div>
-    <div className="mt-4 flex h-44 w-full flex-col space-y-3 rounded-b-lg bg-[#3e7591]  pl-8 text-white">
-      <div className="flex flex-row space-x-4 hover:bg-white hover:bg-opacity-20">
+    <div className="mt-6 flex h-36 w-full flex-col space-y-3 rounded-b-lg bg-[#3e7591]  pl-8 text-white">
+      <div className="flex  flex-row space-x-4 hover:bg-white hover:bg-opacity-20">
         <img src={Logout} />
         <p>Log Out</p>
       </div>
