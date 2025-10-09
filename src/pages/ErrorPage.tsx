17aa055c32c1081bf84a34a1bedd8cb65b441ce0
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-09 20:52:05 +0200

    style: removed the scrollable part

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,8 @@
-import MenuBar from '../components/MenuBar'
 import Error from '../components/Error'
+
 const ErrorPage = () => (
-  <div className="flex min-h-screen flex-col bg-[#265e78]">
-    <MenuBar />
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <Error />
-    </div>
+  <div className="flex h-screen flex-col items-center justify-center bg-[#265e78] overflow-hidden">
+    <Error />
   </div>
 )
 
