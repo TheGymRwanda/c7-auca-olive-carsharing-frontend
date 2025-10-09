0c82026c4f50bd8f8e5eb957fe33f54fd7ae6a17
bdushime <bdushime47@gmail.com>
2025-10-09 20:29:58 +0200

    style: aligning items as the figma file

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -6,8 +6,8 @@ import Stairs from '../assets/Stairs.svg'
 import List from '../assets/List.svg'
 
 const Drawer = () => (
-  <div className="ml-6 mt-8 flex h-80 w-64 flex-1 flex-col items-center justify-center rounded-lg bg-[#3e7591] ">
-    <div className="mt-4 flex h-full w-full flex-col space-y-3  rounded-t-lg bg-[#3e7591] pl-16 text-white">
+  <div className="ml-4 mt-8 flex h-80 w-52 flex-1 flex-col items-center justify-center rounded-lg bg-[#3e7591] ">
+    <div className="mt-4 flex h-full w-full flex-col space-y-3  rounded-t-lg bg-[#3e7591] pl-8 text-white">
       <div className="flex flex-row space-x-4">
         <CarIcon />
         <p>Book A Car</p>
@@ -20,7 +20,7 @@ const Drawer = () => (
     <div className="w-full bg-[#3e7591] px-4">
       <div className="border-b border-gray-300"></div>
     </div>
-    <div className="mb-3 mt-4 flex h-full w-full flex-col space-y-3 bg-[#3e7591] pl-16 text-white ">
+    <div className="mb-3 mt-4 flex h-full w-full flex-col space-y-3 bg-[#3e7591] pl-8 text-white ">
       <div className="flex flex-row space-x-4">
         <img src={CarsM} />
         <p>See My Cars</p>
@@ -37,7 +37,7 @@ const Drawer = () => (
     <div className="w-full bg-[#3e7591]  px-4">
       <div className="border-b border-gray-300"></div>
     </div>
-    <div className="mt-4 flex h-full w-full flex-col space-y-3 rounded-b-lg bg-[#3e7591]  pl-16 text-white">
+    <div className="mt-4 flex h-44 w-full flex-col space-y-3 rounded-b-lg bg-[#3e7591]  pl-8 text-white">
       <div className="flex flex-row space-x-4">
         <img src={Logout} />
         <p>Log Out</p>
--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProfileIcon from '../assets/ProfileIcon'
-import lgHeader from '../assets/lgHeader.png'
+import Ecocar from '../assets/Ecocar.svg'
 
 interface MenuBarProps {
   isDrawerOpen: boolean
@@ -11,13 +11,13 @@ const MenuBar: React.FC<MenuBarProps> = ({ isDrawerOpen, toggleDrawer }) => (
   <div className="relative flex h-16 w-full items-center justify-between rounded-b-2xl bg-[#111827] px-6 text-white shadow-lg">
     <div
       onClick={toggleDrawer}
-      className="cursor-pointer text-xl font-medium transition-all duration-200 hover:text-gray-300"
+      className="cursor-pointer text-xl  transition-all duration-200 hover:text-gray-300"
     >
       {isDrawerOpen ? 'Close' : 'Menu'}
     </div>
 
-    <div className="absolute -bottom-5 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full border-[3px] border-[#0f172a] bg-[#111827] shadow-md">
-      <img src={lgHeader} alt="Logo" className="h-16 w-16" />
+    <div className="absolute -bottom-3 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full border-[3px] border-[#0f172a] bg-[#111827] shadow-md">
+      <img src={Ecocar} alt="Logo" className="h-12 w-16" />
     </div>
 
     <div>
