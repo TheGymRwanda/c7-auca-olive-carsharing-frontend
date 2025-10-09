59173bb09ad5fd6b03c32f1b671ca2d1c02c3726
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-09 21:26:37 +0200

    style: adjusted the screen height

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,7 @@
 import Error from '../components/Error'
 
 const ErrorPage = () => (
-  <div className="flex h-screen flex-col items-center justify-center bg-[#265e78] overflow-hidden">
+  <div className="flex min-h-screen flex-col items-center justify-center bg-[#265e78]">
     <Error />
   </div>
 )
