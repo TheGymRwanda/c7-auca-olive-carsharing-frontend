45f995a698a9801f7789d47e4ab63e5cde7503b9
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-10-09 22:56:38 +0200

    fix: font style on title

--- src/components/MoniShareTitle.tsx
+++ src/components/MoniShareTitle.tsx
@@ -1,7 +1,7 @@
 const MoniShareTitle = () => (
   <h1 className="text-white font-lora text-2xl flex flex-col text-center">
     <span className="font-bold text-5xl">MONI</span>
-    <span className="font-medium text-4xl">share</span>
+    <span className="font-medium text-4xl italic">share</span>
   </h1>
 )
 
