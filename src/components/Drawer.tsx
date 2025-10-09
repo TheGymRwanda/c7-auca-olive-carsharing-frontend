b7cafe358d1a77142c4a75f5f3c69c7316331146
bdushime <bdushime47@gmail.com>
2025-10-09 11:40:55 +0200

    style: fixing menu and drawer functionality

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -3,10 +3,11 @@ import CarIcon from '../assets/CarIcon'
 import CarPlusIcon from '../assets/CarPlusIcon'
 import logout from '../assets/logout.png'
 import stairs from '../assets/stairs.png'
+import list from '../assets/List.png'
 
 const Drawer = () => (
-  <div className="ml-6 mt-8 flex h-80 w-72 flex-1 flex-col items-center justify-center rounded-lg bg-[#3e7591] ">
-    <div className="mt-4 flex h-full w-full flex-col space-y-3 rounded-t-lg bg-[#3e7591] pl-20 text-white">
+  <div className="ml-6 mt-8 flex h-80 w-64 flex-1 flex-col items-center justify-center rounded-lg bg-[#3e7591] ">
+    <div className="mt-4 flex h-full w-full flex-col space-y-3  rounded-t-lg bg-[#3e7591] pl-16 text-white">
       <div className="flex flex-row space-x-4">
         <CarIcon />
         <p>Book A Car</p>
@@ -19,13 +20,13 @@ const Drawer = () => (
     <div className="w-full bg-[#3e7591] px-4">
       <div className="border-b border-gray-300"></div>
     </div>
-    <div className="mb-3 mt-4 flex h-full w-full flex-col space-y-3 bg-[#3e7591] pl-20 text-white ">
+    <div className="mb-3 mt-4 flex h-full w-full flex-col space-y-3 bg-[#3e7591] pl-16 text-white ">
       <div className="flex flex-row space-x-4">
         <img src={carsM} />
         <p>See My Cars</p>
       </div>
       <div className="flex flex-row space-x-4">
-        <CarIcon />
+        <img src={list} />
         <p className="ml-4">My Car&apos;s Bookings</p>
       </div>
       <div className="flex flex-row space-x-4">
@@ -36,7 +37,7 @@ const Drawer = () => (
     <div className="w-full bg-[#3e7591]  px-4">
       <div className="border-b border-gray-300"></div>
     </div>
-    <div className="mt-4 flex h-full w-full flex-col space-y-3 rounded-b-lg bg-[#3e7591]  pl-20 text-white">
+    <div className="mt-4 flex h-full w-full flex-col space-y-3 rounded-b-lg bg-[#3e7591]  pl-16 text-white">
       <div className="flex flex-row space-x-4">
         <img src={logout} />
         <p>Log Out</p>
