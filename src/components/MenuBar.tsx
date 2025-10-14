19d82b66d07e2e90a04e18abc2959329627a9bed
bdushime <bdushime47@gmail.com>
2025-10-14 12:14:56 +0200

    style: changing hardcoded colors to Constants

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -2,17 +2,13 @@ import React from 'react'
 import ProfileIcon from '../assets/ProfileIcon'
 import Ecocar from '../assets/Ecocar.svg'
 import { Link } from 'react-router-dom'
-import { COLORS } from '../constants/colors'
 interface MenuBarProps {
   isDrawerOpen: boolean
   toggleDrawer: () => void
 }
 
 const MenuBar: React.FC<MenuBarProps> = ({ isDrawerOpen, toggleDrawer }) => (
-  <div
-    className="relative flex h-16 w-full items-center justify-between rounded-b-2xl  px-6 text-white shadow-lg"
-    style={{ backgroundColor: COLORS.primary }}
-  >
+  <div className="relative flex h-16 w-full items-center justify-between rounded-b-2xl bg-[#111827] px-6 text-white shadow-lg">
     <div
       onClick={toggleDrawer}
       className="cursor-pointer text-xl  transition-all duration-200 hover:text-gray-300"
@@ -22,8 +18,7 @@ const MenuBar: React.FC<MenuBarProps> = ({ isDrawerOpen, toggleDrawer }) => (
 
     <Link
       to="/"
-      className="absolute -bottom-3 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full border-[3px] border-[#0f172a] shadow-md"
-      style={{ backgroundColor: COLORS.primary }}
+      className="absolute -bottom-3 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full border-[3px] border-[#0f172a] bg-[#111827] shadow-md"
     >
       <img src={Ecocar} alt="Logo" className="h-12 w-16" />
     </Link>
--- src/constants/colors.ts
+++
@@ -1,8 +0,0 @@
-export const COLORS = {
-  primary: '#111827',
-  background: '#111827',
-  text: {
-    primary: '#ffffff',
-    secondary: '#9CA3AF',
-  },
-} as const
