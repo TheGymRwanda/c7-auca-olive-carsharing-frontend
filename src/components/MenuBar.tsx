cc1d4d638de2207005cbacbf0405590bfb834d3f
bdushime <bdushime47@gmail.com>
2025-10-14 10:05:36 +0200

    style: changing hardcoded colors to Constants

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,14 +1,21 @@
 import ProfileIcon from '../assets/ProfileIcon'
 import lgHeader from '../assets/lgHeader.png'
+import { COLORS } from '../constants/colors'
 
 function MenuBar() {
   return (
-    <div className="flex h-16 w-full flex-row gap-8 rounded-b-lg bg-[#111827]">
+    <div
+      className="flex h-16 w-full flex-row gap-8 rounded-b-lg"
+      style={{ backgroundColor: COLORS.primary }}
+    >
       <div className="ml-6 flex h-16 w-4 flex-1 items-center justify-start text-xl text-white">
         Menu
       </div>
 
-      <div className="absolute -bottom-4 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full bg-[#111827] text-white">
+      <div
+        className="absolute -bottom-4 left-1/2 flex h-20 w-20 -translate-x-1/2 items-center justify-center rounded-full  text-white"
+        style={{ backgroundColor: COLORS.primary }}
+      >
         <img src={lgHeader} alt="Header logo" className="h-12 w-12" />
       </div>
 
---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  primary: '#111827',
+  background: '#111827',
+  text: {
+    primary: '#ffffff',
+    secondary: '#9CA3AF',
+  },
+} as const
