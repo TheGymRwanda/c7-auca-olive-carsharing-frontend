8577b14358159bc4f9036816e0837b7e673a87fe
bdushime <bdushime47@gmail.com>
2025-10-15 18:42:14 +0200

    style: fixing all reviews

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -3,7 +3,7 @@ import MainLayout from '../components/MainLayout'
 
 const ErrorPage = () => (
   <MainLayout>
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[#265e78]">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-primary-dark">
       <Error />
     </div>
   </MainLayout>
