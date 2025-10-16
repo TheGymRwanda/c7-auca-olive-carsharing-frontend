4500b1783ddffeb7e6eba84197bb98090a628cd4
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-16 10:43:30 +0200

    fix: removed extra spaces

--- src/assets/TimeIcon.tsx
+++ src/assets/TimeIcon.tsx
@@ -4,22 +4,22 @@ interface TimeIconProps {
   className?: string
 }
 
-const TimeIcon = ({ className }: TimeIconProps): ReactElement =>(
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      stroke="currentColor"
-      className={className}
-    >
-      <circle cx="12" cy="12" r="8.5" />
-      <path d="M9 6L12 12.5L16 10.3636" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M5 12H7" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M17 12H19" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M12 19L12 17" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M12 7L12 5" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  )
+const TimeIcon = ({ className }: TimeIconProps): ReactElement => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke="currentColor"
+    className={className}
+  >
+    <circle cx="12" cy="12" r="8.5" />
+    <path d="M9 6L12 12.5L16 10.3636" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5 12H7" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M17 12H19" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 19L12 17" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 7L12 5" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
 export default TimeIcon
