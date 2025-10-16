bc176fb1a665df09e87b1f2f5bdad3a360eed142
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-16 10:20:32 +0200

    refactor: chevron icons to use function expression

--- src/assets/ChevronDownIcon.tsx
+++ src/assets/ChevronDownIcon.tsx
@@ -4,18 +4,17 @@ interface ChevronDownIconProps {
   className?: string
 }
 
-export function ChevronDownIcon({ className }: ChevronDownIconProps): ReactElement {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      stroke="currentColor"
-    >
-      <path d="M6 9L12 15L18 9" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  )
-}
+const ChevronDownIcon = ({ className }: ChevronDownIconProps): ReactElement => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    stroke="currentColor"
+  >
+    <path d="M6 9L12 15L18 9" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
+export default ChevronDownIcon
