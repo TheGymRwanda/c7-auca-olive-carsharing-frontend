8008641237166bebbc988abedf96d93efe90d17b
bdushime <bdushime47@gmail.com>
2025-10-16 16:05:38 +0200

    style: fixing Homepage font

--- src/components/MoniShareTitle.tsx
+++ src/components/MoniShareTitle.tsx
@@ -1,7 +1,7 @@
 const MoniShareTitle = () => (
-  <h1 className="flex flex-col text-center text-2xl text-white">
-    <span className="font-lora text-4xl font-bold leading-none">MONI</span>
-    <span className="-mt-2 font-lora text-[51px] font-medium italic leading-none">share</span>
+  <h1 className="flex flex-col text-center text-white">
+    <span className="font-lora text-[45.05px] font-bold leading-[51.15px]">MONI</span>
+    <span className="-mt-2 font-lora text-[51.49px] font-medium italic leading-none">share</span>
   </h1>
 )
 
