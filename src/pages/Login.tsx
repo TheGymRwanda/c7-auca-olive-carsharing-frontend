61b569ef6bbb8e64962f75be810067e80589ad16
Jessica Irakoze <jessicairakoze4@gmail.com>
2025-10-24 12:42:31 +0200

    feat: added the login backend functionality and logout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,9 +4,29 @@ import MoniShareTitle from "../components/MoniShareTitle"
 import PasswordIcon from "../assets/Password.svg"
 import Button from "../components/Button"
 import MenuBar from "../components/MenuBar"
+import axios from "axios"
+import { apiUrl } from "../util/apiUrl"
+import { useState } from "react"
 
 const Login = () => {
   const navigate = useNavigate()
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(`${apiUrl}/auth`, {
+        username,
+        password,
+      })
+      const { userId, token } = response.data
+      localStorage.setItem("token", token)
+      localStorage.setItem("userId", userId)
+      navigate("/home")
+    } catch (error) {
+      console.error("Login failed", error)
+    }
+  }
 
   return (
     <div className="min-h-screen">
@@ -20,6 +40,8 @@ const Login = () => {
           <div className="relative">
             <ProfileIcon className="absolute left-4 top-1/2 -translate-y-1/2" />
             <input
+              value={username}
+              onChange={e => setUsername(e.target.value)}
               className="w-full pl-14 py-4 rounded-full placeholder:text-white bg-primary-ocean focus:outline-none"
               type="text"
               placeholder="Username / e-mail"
@@ -33,6 +55,8 @@ const Login = () => {
               className="absolute left-4 top-1/2 -translate-y-1/2"
             />
             <input
+              value={password}
+              onChange={e => setPassword(e.target.value)}
               className="w-full pl-14 py-4 rounded-full placeholder:text-white bg-primary-ocean focus:outline-none"
               type="password"
               placeholder="Password"
@@ -41,7 +65,7 @@ const Login = () => {
         </div>
 
         <div className="mt-8 flex w-full max-w-md flex-col items-center">
-          <Button variant="primary" fullWidth onClick={() => navigate("/")}>
+          <Button variant="primary" fullWidth onClick={handleLogin}>
             Log In
           </Button>
         </div>
